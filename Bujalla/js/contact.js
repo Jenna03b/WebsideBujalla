10745b79fafacf565c23dbadc5a2c57b3b1511ae
Jennifer <119788639+Jenna03b@users.noreply.github.com>
2023-12-02 14:10:11 +0100

    Add files via upload

---
+++ Bujalla/js/contact.js
@@ -0,0 +1,51 @@
+
+function validateContact() {
+    var valid = true;	
+    $(".information .demoInputBox").css('background-color','');
+    $(".information .info").html('');
+    if(!$("#userName").val()) {
+        $("#userName-info").html("(required)");
+        $("#userName").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#userEmail").val()) {
+        $("#userEmail-info").html("(required)");
+        $("#userEmail").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+        $("#userEmail-info").html("(invalid)");
+        $("#userEmail").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#subject").val()) {
+        $("#subject-info").html("(required)");
+        $("#subject").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#content").val()) {
+        $("#content-info").html("(required)");
+        $("#content").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    return valid;
+}
+
+function sendContact() {
+    var valid;	
+    valid = validateContact();
+    if(valid) {
+        jQuery.ajax({
+            url:("C:/Users/User/Desktop/Firma/Bujalla/php/contact.php"),
+            data:'userName='+$("#userName").val()+'&userEmail='+
+            $("#userEmail").val()+'&subject='+
+            $("#subject").val()+'&content='+
+            $(content).val(),
+            type: "POST",
+            success:function(data){
+                $("#mail-status").html(data);
+            },
+            error:function (){}
+        });
+    }
+}
